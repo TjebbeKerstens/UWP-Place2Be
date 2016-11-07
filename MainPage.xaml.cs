88480acc2515b2dc0538e10d620df7d3c1459f22
Tjebbe Kerstens <t.kerstens@live.nl>
2016-11-07 13:53:28 +0100

    Added working map, which zooms on the users current location, cant continue with speech yet because my microphone is not configured yet

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.SpeechRecognition;
+using Windows.Storage.BulkAccess;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -22,9 +30,81 @@ namespace Place2Be
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Geolocator geolocator;
+        Geoposition geoposition;
         public MainPage()
         {
             this.InitializeComponent();
+
+            geolocator = new Geolocator();
+            geolocator.DesiredAccuracyInMeters = 10;
+            geolocator.ReportInterval = 0;
+
+            setPositionAsync();
+        }
+
+        private async void setPositionAsync()
+        {
+            // Get current user position
+            geoposition = await geolocator.GetGeopositionAsync();
+            string latitude = geoposition.Coordinate.Latitude.ToString("0.0000000000");
+            string Longitude = geoposition.Coordinate.Longitude.ToString("0.0000000000");
+            string Accuracy = geoposition.Coordinate.Accuracy.ToString("0.0000000000");
+
+            // Write position to usable format
+            BasicGeoposition basicGeoposition = new BasicGeoposition();
+            basicGeoposition.Latitude = Convert.ToDouble(latitude);
+            basicGeoposition.Longitude = Convert.ToDouble(Longitude);
+
+            // Create geopoint for position
+            Geopoint current = new Geopoint(basicGeoposition);
+            
+            // Calculate bounding box containing everything
+            GeoboundingBox geoboundingBox = new GeoboundingBox(basicGeoposition, basicGeoposition);
+
+            // Zoom to current position
+            await Map.TrySetViewAsync(current);
+            Map.TryZoomToAsync(16);
+
+            // Create mapicon
+            MapIcon mapIcon = new MapIcon();
+            mapIcon.Image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/MapPin.png"));
+            mapIcon.Location = current;
+
+            // Wait for the animation to finish before adding the mp
+            await Task.Delay(1000);
+            Map.MapElements.Add(mapIcon);
+        }
+
+        private async void OpenUI_Click(object sender, RoutedEventArgs e)
+        {
+            SpeechRecognizer recognizer = new SpeechRecognizer();
+
+            SpeechRecognitionTopicConstraint topicConstraint
+                    = new SpeechRecognitionTopicConstraint(SpeechRecognitionScenario.Dictation, "Development");
+
+            recognizer.Constraints.Add(topicConstraint);
+            await recognizer.CompileConstraintsAsync(); // Required
+
+            try
+            {
+                // Open the UI.
+                var results = await recognizer.RecognizeWithUIAsync();
+                if (results.Confidence != SpeechRecognitionConfidence.Rejected)
+                {
+                    Debug.WriteLine(results.Text);
+                    // No need to call 'Voice.Say'. The control speaks itself.
+                }
+                else
+                {
+                    Debug.WriteLine("Sorry, I did not get that.");
+                }
+            }
+            catch
+            {
+                MessageDialog dialog = new MessageDialog("Please go to Settings > Speech and configure your microphone for Speech recognition", "Speech recoginition not set up");
+                dialog.ShowAsync();
+            }
         }
     }
 }
