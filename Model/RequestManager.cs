45066359a97a7018dbe2cf3cb43912b9986aed3f
Orlando Smits <orlandosmits@live.nl>
2016-11-20 17:19:44 +0100

    Started with RequestManager

---
+++ Model/RequestManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.Web.Http;
+
+namespace Place2Be.Model
+{
+    public class RequestManager
+    {
+        private static RequestManager _instance;
+        private HttpClient client = new HttpClient();
+
+        string api_key = "AIzaSyB40JmMGhRhBwqzOK-EvTVQ020TvSLPL_I";
+
+        private RequestManager() { }
+
+        public static RequestManager GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new RequestManager();
+            }
+
+            return _instance;
+        }
+
+        public async Task<string> RetrieveNearbyPlace()
+        {
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(5000);
+            try
+            {
+                Uri uri = new Uri($"https://maps.googleapis.com/maps/api/place/nearbysearch/");
+            }
+        }
+    }
+}
