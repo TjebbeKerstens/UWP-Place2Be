2d204f686b8f8f24ebc3941cd9f52c892330db3e
Tjebbe Kerstens <t.kerstens@live.nl>
2016-12-22 20:52:01 +0100

    List now filling correctly

--- Model/SimpleLocation.cs
+++ Model/SimpleLocation.cs
@@ -8,9 +8,9 @@ namespace Place2Be.Model
 {
     class SimpleLocation
     {
-        private int Id { get; set; }
-        private string Name { get; set; }
-        private string Address { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
 
         public SimpleLocation(int id, string name, string address)
         {
